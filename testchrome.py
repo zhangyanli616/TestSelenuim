46df7dd19d53aa5fc177f6b0d632190e21981833
zhangyanli <zhangyanli@taoche.com>
2020-02-22 19:32:57 +0800

    003 versioin

--- testchrome.py
+++ testchrome.py
@@ -1,10 +1,10 @@
 
 # coding = utf-8
 from selenium import webdriver
-# chrome_option = webdriver.ChromeOptions()
-# browser = webdriver.Chrome(options = chrome_option)
-# browser.get("https://www.baidHHHu.com")
+chrome_option = webdriver.ChromeOptions()
+browser = webdriver.Chrome(options = chrome_option)
+browser.get("https://www.baidu.com")
 
 
-wd = webdriver.Chrome('C:\\Users\\zhangyanli\\AppData\\Local\\Programs\\Python\\Python37\\chromedriver.exe')
-wd.get('https://www.baidu.com')
+# wd = webdriver.Chrome('C:\\Users\\zhangyanli\\AppData\\Local\\Programs\\Python\\Python37\\chromedriver.exe')
+# wd.get('https://www.baidu.com')
